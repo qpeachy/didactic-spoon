7d7b6cc2cfcba90973412b6745362def00971d61
Mathy Pale <mpale@z2r11p2.42lyon.fr>
2021-08-26 18:20:51 +0200

    over

---
+++ C_06/ex03/ft_sort_params.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpale <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/26 09:01:40 by mpale             #+#    #+#             */
+/*   Updated: 2021/08/26 18:18:51 by mpale            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_sort_int_tab(char **tab, int size)
+{
+	int	j;
+
+	j = 1;
+	while (j < size - 1)
+	{
+		if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+		{
+			ft_swap(&tab[j], (&tab[j + 1]));
+			j = 0;
+		}
+		j++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	ft_sort_int_tab(argv, argc);
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+}
